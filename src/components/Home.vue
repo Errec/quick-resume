ee890fb7db4449f2cb23148741278856c7797731
Raniro Coelho <errecme@gmail.com>
2019-01-15 11:24:18 -0200

    Add header component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,8 @@
 <template lang="pug">
   div.home
     h1 Select a template
-    router-link(to="template-a")
+    //- router-link(to="template-a")
+    router-link(to="header")
       button.home__btn-next Next
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
+import Header from '@/components/Header'
 import TemplateA from '@/components/templates/TemplateA'
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/template-a',
-      name: 'template-a',
-      component: TemplateA
-    }
-  ]
+    mode: 'history',
+    routes: [{
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/template-a',
+            name: 'template-a',
+            component: TemplateA
+        },
+        {
+            path: '/header',
+            name: 'header',
+            component: Header
+        }
+    ]
 })
